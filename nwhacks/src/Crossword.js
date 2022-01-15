c8b7d2173a420c9d4246b5a6dafde90d1fc5675b
Redorangegamez <canvanzeng@gmail.com>
2022-01-15 14:04:07 -0800

    added empty functions and parameters to crossword class

--- nwhacks/src/Crossword.js
+++ nwhacks/src/Crossword.js
@@ -1,5 +1,22 @@
 class Crossword {
     constructor(words) {
         this.words = words;
+        const MAXSIZE = 10;
+        this.grid = [...Array(MAXSIZE)].map(e => Array(MAXSIZE));
+        this.isFilled = [...Array(MAXSIZE)].map(e => Array(MAXSIZE).fill(false));
     }
+
+    generate() {
+        return
+    }
+
+    static findCorrectGrids(c1, c2) {
+        return
+    }
+
+    fillCell(letter, x, y) {
+        this.grid[x][y] = letter
+        this.isFilled[x][y] = true
+    }
+
 }
