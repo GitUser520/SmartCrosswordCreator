d0d84574b2380b25c21ee790eff6ab0bc4f54a89
Peiyan Yang <iforgotpy@gmail.com>
2022-01-16 10:12:10 -0800

    connected buttons

--- nwhacks/src/AppBarComponent.js
+++ nwhacks/src/AppBarComponent.js
@@ -8,85 +8,84 @@ import {
 import MenuIcon from "@mui/icons-material/Menu";
 import Crossword from "./Crossword.js";
 
-export const AppBarComponent = (props) => {
-    return (
-        <CompleteAppBar state={props.state} />
-    );
+// export const AppBarComponent = (props) => {
+//     return (
+//         <CompleteAppBar state={props.state} />
+//     );
+// }
+
+class CompleteAppBar extends React.Component {
+
+    checkSolution() {
+        this.props.checkSolution();
+    }
+
+    displaySolution() {
+        this.props.displaySolution();
+    }
+
+    newGame() {
+        this.props.newGame();
+    }
+
+    render() {
+        return (
+            <Box>
+                <AppBar justifyContent={"center"}>
+                    <Toolbar>
+                        <Link style={{textDecoration: 'none'}} to="/">
+                            <IconButton size={"large"}
+                                        edge={"start"}
+                                        children={<MenuIcon/>}>
+                            </IconButton>
+                        </Link>
+                        <Button
+                            variant={"contained"}
+                            color={"secondary"}
+                            onClick={() => {
+                                alert("This should be the login button.");
+                            }}
+                        >
+                            Login
+                        </Button>
+                        <Typography variant={"h5"} align="center" style={{width: "100%", alignItems: "center"}}>
+                            Smart Crossword Creator
+                        </Typography>
+                        <Button
+                            variant={"contained"}
+                            color={"secondary"}
+                            onClick={() => {
+                                this.checkSolution();
+                            }}
+                        >
+                            Check
+                        </Button>
+                        <Button
+                            variant={"contained"}
+                            color={"secondary"}
+                            onClick={() => {
+                                this.displaySolution();
+                            }}
+                        >
+                            Display
+                        </Button>
+                        <Button
+                            variant={"contained"}
+                            color={"secondary"}
+                            onClick={() => {
+                                this.newGame();
+                            }}
+                        >
+                            New Game
+                        </Button>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+        );
+    }
 }
 
-function CompleteAppBar(props) {
-
-    const [state, setState] = useState(props.state);
-
-    const checkSolution = () => {
-
-    }
-
-    const displaySolution = () => {
-
-    }
-
-    const newGame = () => {
-
-    }
-
-    return (
-        <Box>
-            <AppBar justifyContent={"center"}>
-                <Toolbar>
-                    <Link style={{ textDecoration: 'none' }} to="/">
-                        <IconButton size={"large"}
-                                    edge={"start"}
-                                    children={<MenuIcon />}>
-                        </IconButton>
-                    </Link>
-                    <Button
-                        variant={"contained"}
-                        color={"secondary"}
-                        onClick={() => {
-                            alert("This should be the login button.");
-                        }}
-                    >
-                        Login
-                    </Button>
-                    <Typography variant={"h5"} align="center" style={{width: "100%", alignItems: "center"}}>
-                        Smart Crossword Creator
-                    </Typography>
-                    <Button
-                        variant={"contained"}
-                        color={"secondary"}
-                        onClick={() => {
-                            // state.check();
-                            // alert("This should be the check button." + state.num);
-                        }}
-                    >
-                        Check
-                    </Button>
-                    <Button
-                        variant={"contained"}
-                        color={"secondary"}
-                        onClick={() => {
-                            // state.solve();
-                            // alert("This should be the display button." + state.num);
-                        }}
-                    >
-                        Display
-                    </Button>
-                    <Button
-                        variant={"contained"}
-                        color={"secondary"}
-                        onClick={() => {
-                            // state.newGame();
-                            // alert("This should be the new game button." + state.num);
-                        }}
-                    >
-                        New Game
-                    </Button>
-                </Toolbar>
-            </AppBar>
-        </Box>
-    );
-}
+export default CompleteAppBar;
 
 
 
