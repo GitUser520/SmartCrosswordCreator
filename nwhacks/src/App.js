b09a9eb49e3094a673786fe5906e7bb09f3a1c3a
emath01 <emath01@students.cs.ubc.ca>
2022-01-15 18:40:20 -0800

    Revert to RedOrangeGamez's commit

--- nwhacks/src/App.js
+++ nwhacks/src/App.js
@@ -1,16 +1,52 @@
+import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import { CrosswordComponentReact } from "./CrosswordComponent.js";
+
+let i = 0
 
 function App() {
-  return (
-    <div className="App">
-        <CrosswordComponentReact grid={[["t","s","z"],["a","a","b"],["c","a","b"]]} />
-    </div>
-  );
+    return (
+        <body>
+        <h1 className = "App">
+            Your Personalized Crossword
+        </h1>
+        <div className="App">
+            <CrosswordComponent grid={[["t","s","z"],["a","a","b"],["c","a","b"]]} />
+        </div>
+        <hr/>
+        <h2> Across </h2>
+        <div className="App">
+            <HintsComponent grid={[["t","s","z"],["a","a","b"],["c","a","b"]]} />
+        </div>
+        <h2> Below </h2>
+        <div className="App">
+            <HintsComponent grid={[["t","s","z"],["a","a","b"],["c","a","b"]]} />
+        </div>
+
+        </body>
+    );
+}
+
+class CrosswordComponent extends React.Component {
+    render() {
+        var indents = [];
+        for (var i = 0; i < this.props.grid.length; i++) {
+            for (var j = 0; j < this.props.grid[0].length; j++) {
+                //indents.push(<div key={i*3 + j}>{this.props.grid[i][j]}</div>);
+                indents.push(<input type="text" name = "fname" id="fname" maxLength="1" key={i*3 + j} className = "input-box"/>);
+            }
+            indents.push(<br/>)
+        }
+        return indents;
+    }
+}
+
+class HintsComponent extends React.Component {
+    render() {
+        var indents = [];
+        return indents;
+    }
 }
 
 
-
 export default App;
-
