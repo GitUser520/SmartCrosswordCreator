2bfd50652d962a4b4f4ba0d4fcc6cf284ea59c45
emath01 <emath01@students.cs.ubc.ca>
2022-01-15 18:15:48 -0800

    Refactored code to use Material UI.

--- nwhacks/src/App.js
+++ nwhacks/src/App.js
@@ -1,28 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-
-let i = 0
+import { CrosswordComponentReact } from "./CrosswordComponent.js";
 
 function App() {
   return (
     <div className="App">
-      <CrosswordComponent grid={[["t","s","z"],["a","a","b"],["c","a","b"]]} />
+        <CrosswordComponentReact grid={[["t","s","z"],["a","a","b"],["c","a","b"]]} />
     </div>
   );
 }
 
-class CrosswordComponent extends React.Component {
-    render() {
-        var indents = [];
-        for (var i = 0; i < this.props.grid.length; i++) {
-            for (var j = 0; j < this.props.grid[0].length; j++) {
-                indents.push(<div key={i*3 + j}>{this.props.grid[i][j]}</div>);
-            }
-        }
-        return indents;
-    }
-}
+
 
 export default App;
 
