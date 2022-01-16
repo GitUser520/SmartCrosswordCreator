aeb6d009f4514aae040289062cf7e413f408c74a
emath01 <emath01@students.cs.ubc.ca>
2022-01-15 18:36:45 -0800

    Revert "Added basic template for UI (header, grid-like grid with editable elements, across and below titles)"
    This reverts commit ec39ef47

--- nwhacks/src/App.js
+++ nwhacks/src/App.js
@@ -1,16 +1,13 @@
 import './App.css';
 import React from 'react';
 import { CrosswordComponentReact } from "./CrosswordComponent.js";
-import { HomePage } from "./HomePage.js";
 
 function App() {
-      return (
-            <div className="App">
-                <HomePage />
-                <br/>
-                <CrosswordComponentReact grid={[["t","s","z"],["a","a","b"],["c","a","b"]]} />
-            </div>
-      );
+  return (
+    <div className="App">
+        <CrosswordComponentReact grid={[["t","s","z"],["a","a","b"],["c","a","b"]]} />
+    </div>
+  );
 }
 
 
