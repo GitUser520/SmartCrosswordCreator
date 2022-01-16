0fb2177637542ac7a954192964e8f19d72969049
emath01 <emath01@students.cs.ubc.ca>
2022-01-15 20:15:25 -0800

    Menu Bar 1.0

--- nwhacks/src/HomePage.js
+++ nwhacks/src/HomePage.js
@@ -1,11 +1,10 @@
 import React from "react";
 import {
+    Box,
     AppBar, Button, Toolbar,
     IconButton, Typography
 } from "@mui/material";
-import {
-    MenuIcon
-} from "@mui/icons-material/Menu"
+import MenuIcon from "@mui/icons-material/Menu";
 
 export const HomePage = () => {
     return (
@@ -15,25 +14,33 @@ export const HomePage = () => {
 
 function CompleteAppBar() {
     return (
-        <AppBar>
-            <Toolbar>
-                <IconButton
-                    size={"medium"}
-                    edge={"start"}
-                >
-                </IconButton>
-                    <MenuIcon />
-                <Button
-                    variant={"contained"}
-                    color={"secondary"}
-                    onClick={() => {
-                       alert("This should be the login button.");
-                    }}
-                >
-                    Login
-                </Button>
-            </Toolbar>
-        </AppBar>
+        <Box padding={2}>
+            <AppBar>
+                <Toolbar>
+                    <IconButton
+                        size={"large"}
+                        edge={"start"}
+                        onClick={() => {
+                            alert("This should be the menu button.");
+                        }}
+                        children={<MenuIcon />}
+                    >
+                    </IconButton>
+                    <Button
+                        variant={"contained"}
+                        color={"secondary"}
+                        onClick={() => {
+                            alert("This should be the login button.");
+                        }}
+                    >
+                        Login
+                    </Button>
+                    <Typography variant={"h5"}>
+                        Placeholder for Crossword Name
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
     );
 }
 
