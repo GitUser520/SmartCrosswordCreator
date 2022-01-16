a5b6f04148272a72fb710001f4642ec0d5d6044c
Peiyan Yang <iforgotpy@gmail.com>
2022-01-15 16:42:08 -0800

    crossword class

--- nwhacks/src/App.js
+++ nwhacks/src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+let i = 0
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CrosswordComponent grid={[["t","s","z"],["a","a","b"],["c","a","b"]]} />
     </div>
   );
 }
 
+class CrosswordComponent extends React.Component {
+    render() {
+        var indents = [];
+        for (var i = 0; i < this.props.grid.length; i++) {
+            for (var j = 0; j < this.props.grid[0].length; j++) {
+                indents.push(<div key={i*3 + j}>{this.props.grid[i][j]}</div>);
+            }
+        }
+        return indents;
+    }
+}
+
 export default App;
+
